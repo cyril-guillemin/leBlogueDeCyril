d95f38f106dbd7ff42ca6c27d3c638530028f8fa
cyril-guillemin <cyrgui496@gmail.com>
2024-01-10 05:53:07 +0100

    modif of contact form in general

--- footer.php
+++ footer.php
@@ -4,13 +4,13 @@ echo '
             <h3 class="footer-h3">Contact</h3>
             <form class="footer-form" action="processing.php" method="post">
                 <label class="footer-form-label" for="email">Email</label>
-                <input class="footer-form-input" type="email" id="email" name="user_email" autocomplete="off">
+                <input class="footer-form-input" type="email" id="email" name="user_email" required autocomplete="off">
 
                 <label class="footer-form-label" for="subject">Sujet</label>
-                <input class="footer-form-input" type="text" id="subject" name="user_subject" autocomplete="off">
+                <input class="footer-form-input" type="text" id="subject" name="user_subject" required autocomplete="off">
 
                 <label class="footer-form-label" for="message">Message</label>
-                <textarea class="footer-form-textarea" id="message" name="user_message" autocomplete="off"></textarea>
+                <textarea class="footer-form-textarea" id="message" name="user_message" required autocomplete="off"></textarea>
 
                 <div class="footer-form-div">
                     <button class="footer-form-div-button" type="submit">Envoyer</button>
--- processing.php
+++ processing.php
@@ -3,12 +3,6 @@
     $subject = $_POST["user_subject"];
     $message = $_POST["user_message"];
     $headers .= $_POST["user_email"];
-
-    if(mail($to,$subject,$message,$headers)) {
-        echo "Message envoyé avec succès !";
-    }
-    else
-    {
-        echo "Erreur. Le message ne peut pas être envoyé.";
-    }
+    mail($to,$subject,$message,$headers);
+    header('location: https//www.lebloguedecyril.fr');
 ?>
