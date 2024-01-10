238d6e3f0b76efc6fda117de1361cc5bfe1129d7
cyril-guillemin <cyrgui496@gmail.com>
2024-01-10 21:39:23 +0100

    improved form contact

--- processing.php
+++ processing.php
@@ -10,25 +10,64 @@
         if(isset($_POST["user_firstName"]) && isset($_POST["user_lastName"]) && isset($_POST["user_subject"]) && isset($_POST["user_message"]) && isset($_POST["user_email"])) {
             $firstName = stripslashes(trim($_POST["user_firstName"]));
             $lastName = stripslashes(trim($_POST["user_lastName"]));
+            $email = stripslashes(trim($_POST["user_email"]));
             $subject = stripslashes(trim($_POST["user_subject"]));
             $message = stripslashes(trim($_POST["user_message"]));
-            $headers .= stripslashes(trim($_POST["user_email"]));
             $to = "message@lebloguedecyril.fr";
 
-            if(mail($to, $subject, $message, $headers)) {
+            if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $headers .= stripslashes(trim($_POST["user_email"]));
+                if(mail($to, $subject, $message, $headers)) {
+                    echo '
+                    <div class="div">
+                        <section class="div-section">
+                            <h2 class="div-section-h2">Bonjour ' . $firstName . ' ' . $lastName . ' !</h2>
+                            <p class="div-section-p">Merci à l\'attention que vous portez pour mon blogue !!!</p>
+                            <p class="div-section-noProblem_p">
+                                Votre message à bien été envoyé ! &#128540;
+                            </p>
+                        </section>
+                    </div>';
+                }
+                else {
+                    /* Sinon c'est que le pb provient de la variable $to */
+                    echo '
+                        <div class="div">
+                            <section class="div-section">
+                                <h2 class="div-section-h2">Bonjour ' . $firstName . ' ' . $lastName . ' !</h2>
+                                <p class="div-section-p">Merci à l\'attention que vous portez pour mon blogue !!!</p>
+                                <p class="div-section-div-problem_p">
+                                    Un problème est survenu, veuillez réessayer plus tard.
+                                </p>
+                            </section>
+                        </div>';
+                }
+            } 
+            else {
                 echo '
                 <div class="div">
                     <section class="div-section">
                         <h2 class="div-section-h2">Bonjour ' . $firstName . ' ' . $lastName . ' !</h2>
-                        <p class="div-section-p">
-                            Votre message à bien été envoyé &#128540;
+                        <p class="div-section-p">Merci à l\'attention que vous portez pour mon blogue !!!</p>
+                        <p class="div-section-div-problem_p">
+                                Vous devez entrez un email valid.
                         </p>
                     </section>
                 </div>';
             }
+
         }
         else {
-            echo 'Tout les champs du formlaire sont requis';
+            echo '
+                <div class="div">
+                    <section class="div-section">
+                        <h2 class="div-section-h2">Bonjour ' . $firstName . ' ' . $lastName . ' !</h2>
+                        <p class="div-section-p">Merci à l\'attention que vous portez pour mon blogue !!!</p>
+                        <p class="div-section-div-problem_p">
+                            Tout les champs de formulaire doivent être renseigner.
+                        </p>
+                    </section>
+                </div>';
         }
         ?>
         <?php include 'footer.php';?>
